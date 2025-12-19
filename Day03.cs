82d9b5920be21971e34493c0fdaad69bf659e05a
junassroden <cabanerojhoas@gmail.com>
2025-12-19 15:01:28 +0800

    Add files via upload

---
+++ Day03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day03
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
