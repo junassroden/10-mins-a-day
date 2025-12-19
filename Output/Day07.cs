974e2d892de1c8119ec3bdefdc0ac69b9d3c2be3
junassroden <cabanerojhoas@gmail.com>
2025-12-19 16:14:22 +0800

    Add files via upload

---
+++ Output/Day07.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day07
+    {
+        public static void Main(string[] args)
+        {
+        Console.Write("Enter two numbers separated by space: ");
+        var nums = Console.ReadLine().Split();
+        Console.WriteLine($"Swapped Numbers:\n{nums[1]}\n{nums[0]}");
+        }
+    }
+}
