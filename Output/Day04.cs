e4e6a3f98afe1da361d48fa7bff07fac496b8ff0
junassroden <cabanerojhoas@gmail.com>
2025-12-19 15:05:27 +0800

    Add files via upload

---
+++ Output/Day04.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day03
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            if(number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
