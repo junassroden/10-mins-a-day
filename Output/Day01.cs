3c0607a9baa8db41d91a90ef67ec4fcece2410db
junassroden <cabanerojhoas@gmail.com>
2025-12-19 14:57:23 +0800

    Add files via upload

---
+++ Output/Day01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day01
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
