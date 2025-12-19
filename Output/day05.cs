33607fcc4215f5a3ecba8cbf56141e50b088a81e
junassroden <cabanerojhoas@gmail.com>
2025-12-19 16:02:41 +0800

    Add files via upload

---
+++ Output/day05.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day05
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int max = Math.Max(num1, num2);
+            Console.WriteLine("Max number: " + max);
+        }
+    }
+}
