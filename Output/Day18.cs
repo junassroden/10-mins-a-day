834d4c8d6aaf59e77202a9651dcb0f075e3b7a2a
Jhonas Cabanero <cabanerojhoas@gmail.com>
2025-12-20 12:16:03 +0800

    Add files via upload

---
+++ Output/Day18.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace TenMinsADay
+{
+    public class Day18
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine().Trim();
+            string reversed = new string(number.Reverse().ToArray());
+            if(reversed == number)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+        }
+    }
+}
