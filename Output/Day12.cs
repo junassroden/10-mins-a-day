728a957be172c28c88832be9cfe7892edf8ba5d0
Jhonas Cabanero <cabanerojhoas@gmail.com>
2025-12-20 10:58:00 +0800

    Add files via upload

---
+++ Output/Day12.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day12
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("Enter number: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of first 10 numbers is" + sum);
+        }
+    }
+}
