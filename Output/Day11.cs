0ce12fbccd7354cd55adeb872cd5693db0c60791
Jhonas Cabanero <cabanerojhoas@gmail.com>
2025-12-20 10:53:20 +0800

    Add files via upload

---
+++ Output/Day11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TenMinsADay
+{
+    public class Day11
+    {
+        public static void Main(string[] args)
+        {
+            int [] numbers = new int[10];
+            int i = 0;
+            do
+            {
+                Console.Write("Enter number: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+                i++;
+            }while(i < 10);
+            Console.Write("Numbers: ");
+            foreach(int n in numbers)
+            {
+                Console.Write($"{n} ");
+            }
+        }
+    }
+}
