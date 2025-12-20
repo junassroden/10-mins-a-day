28dff8b9aff01b8f4a0ddb1a66a0beb3350d372b
Jhonas Cabanero <cabanerojhoas@gmail.com>
2025-12-20 12:09:23 +0800

    Add files via upload

---
+++ Output/Day17.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace TenMinsADay
+{
+    public class Day17
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int input = int.Parse(Console.ReadLine());
+            string convert = input.ToString();
+            int length = convert.Length;
+            Console.Write(length);
+        }
+    }
+}
