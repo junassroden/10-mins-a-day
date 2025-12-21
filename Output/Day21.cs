ac20380216a2143d2fd17ac2390996de9debb949
Jhonas Cabanero <cabanerojhoas@gmail.com>
2025-12-21 13:02:14 +0800

    Add files via upload

---
+++ Output/Day21.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace TenMinsADay
+{
+    public class Day21
+    {
+        public static void Main(string[] args)
+        {
+            bool isPrime = true;  
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int d = 2; d < n; d++)
+            {
+                if (n % d == 0)    
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                Console.WriteLine($"{n} is a prime number.");
+            else
+                Console.WriteLine($"{n} is not a prime number.");
+        }
+    }
+}
