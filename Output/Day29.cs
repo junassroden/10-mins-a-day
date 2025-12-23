51e0f4f740cfb95d0ae0c80458e3e7bace85e3fd
Jhonas Cabanero <cabanerojhoas@gmail.com>
2025-12-23 22:07:31 +0800

    Add files via upload

---
+++ Output/Day29.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace TenMinsADay
+{
+    public class Day29
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a letter: ");
+            string let = Console.ReadLine().ToLower();
+
+            bool contains = Regex.IsMatch(let, "^[aeiou]$");
+            if (contains)
+            {
+                Console.Write("Vowel");
+            }
+            else
+            {
+                Console.Write("Consonant");
+            }
+        }
+    }
+}
