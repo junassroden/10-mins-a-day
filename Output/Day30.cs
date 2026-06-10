44e04e081b48e7d198cae9088e6d05bd422b9ed6
Jhonas Cabanero <cabanerojhoas@gmail.com>
2026-06-10 08:22:53 +0800

    Create Day30.cs

---
+++ Output/Day30.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace TenMinsADay
+{
+    public class Day30
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine();
+
+            int largest = number.Max(c => c - '0');
+            Console.WriteLine("Largest digit: " + largest);
+        }
+    }
+}
